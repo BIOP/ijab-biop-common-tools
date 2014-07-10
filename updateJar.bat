c97193c286139e5a089c799ad8c7d996843883ad
Olivier Burri <olivier.burri@epfl.ch>
2014-07-10 11:13:50 +0200

    Added new and improved updateJar.bat

--- updateJar.bat
+++ updateJar.bat
@@ -1,1 +1,38 @@
-jar uf BIOP_Common_Tools.jar plugins.config icons *.ijm test.txt
+@ECHO OFF
+setlocal enabledelayedexpansion
+
+REM Set name of ActionBar
+set actionBarName=BIOP_Common_Tools
+
+
+ECHO Packing ActionBar: "%actionBarName%"
+
+REM Get the version of the ActionBar
+
+git describe --abbrev=0 --tags > tmpFile
+set /p version= < tmpFile
+del tmpFile
+
+ECHO Version: "%version%"
+
+set finalName=%actionBarName%_%version%.jar
+
+echo Final Name: "%finalName%"
+
+REM Create the plugins.config file from the plugins.config.defaults and replace the 
+REM 'actionBarName' text with the complete name of the actionbar
+
+ECHO Getting string from plugins.config.default
+set /p configString= < plugins.config.default
+call set configString=%%configString:ActionBarName=!finalName!%%
+
+ECHO Writing 'plugins.config' file...
+echo %configString% >> plugins.config
+
+REM Create JAR File
+ECHO Creating JAR File
+jar cf %finalName% plugins.config icons *.ijm
+del plugins.config
+ECHO Done.
+
+PAUSE
