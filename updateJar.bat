fc3c19277ee8fc0dad48eba22c6895b035bc1cca
Olivier Burri <olivier.burri@epfl.ch>
2014-07-10 14:00:34 +0200

    Corrected jar creator... not working with > character in plugins.config

--- updateJar.bat
+++ updateJar.bat
@@ -19,20 +19,9 @@ set finalName=%actionBarName%.jar
 
 echo Final Name: "%finalName%"
 
-REM Create the plugins.config file from the plugins.config.defaults and replace the 
-REM 'actionBarName' text with the complete name of the actionbar
-
-ECHO Getting string from plugins.config.default
-set /p configString= < plugins.config.default
-call set configString=%%configString:ActionBarName=!finalName!%%
-
-ECHO Writing 'plugins.config' file...
-echo %configString% >> plugins.config
-
 REM Create JAR File
 ECHO Creating JAR File
 jar cf %finalName% plugins.config icons *.ijm
-del plugins.config
 ECHO Done.
 
 PAUSE
