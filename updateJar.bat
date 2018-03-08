250f69f3a620e3735aff3455f8cdd309ebc00ed1
Olivier Burri <olivier.burri@epfl.ch>
2018-03-08 11:27:29 +0100

    Moves Common Tools to new location

--- updateJar.bat
+++ updateJar.bat
@@ -5,21 +5,12 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 REM Set name of ActionBar
 set actionBarName=BIOP_Common_Tools
 
-copy "C:\Fiji\plugins\ActionBar\BIOP_Common_Tools.ijm" BIOP_Common_Tools.ijm
-copy "C:\Fiji\plugins\ActionBar\channelManipToolsSubBar.ijm" channelManipToolsSubBar.ijm
+copy "C:\Fiji\plugins\ActionBar\Debug\BIOP_Common_Tools.ijm" BIOP_Common_Tools.ijm
+copy "C:\Fiji\plugins\ActionBar\Debug\channelManipToolsSubBar.ijm" channelManipToolsSubBar.ijm
 
 ECHO Packing ActionBar: "%actionBarName%"
 
-REM Get the version of the ActionBar
-git describe --abbrev=0 --tags > tmpFile
-set /p version= < tmpFile
-del tmpFile
-
-ECHO Version: "%version%"
-
 set finalName=%actionBarName%.jar
-
-echo Final Name: "%finalName%"
 
 REM Create JAR File
 ECHO Creating JAR File
